88785e04cca551b2170af03b337917796d98dc78
WallaceBarrosDev <w.wallacebarro.cruz@gmail.com>
2026-04-11 15:48:10 -0300

    1018

--- src/main.c
+++ src/main.c
@@ -1,9 +1,30 @@
 #include <stdio.h>
 
 int main(void) {
-  int tempo, velocidadeMedia;
-  double gastoTotal;
-  scanf("%i %i", &tempo, &velocidadeMedia);
-  printf("%.3lf\n", (tempo * velocidadeMedia / 12.0));
+  int numero;
+  scanf("%d", &numero);
+
+  printf("%d\n", numero);
+  printf("%d nota(s) de R$ 100,00\n", numero / 100);
+  numero %= 100;
+
+  printf("%d nota(s) de R$ 50,00\n", numero / 50);
+  numero %= 50;
+
+  printf("%d nota(s) de R$ 20,00\n", numero / 20);
+  numero %= 20;
+
+  printf("%d nota(s) de R$ 10,00\n", numero / 10);
+  numero %= 10;
+
+  printf("%d nota(s) de R$ 5,00\n", numero / 5);
+  numero %= 5;
+  
+  printf("%d nota(s) de R$ 2,00\n", numero / 2);
+  numero %= 2;
+  
+  printf("%d nota(s) de R$ 1,00\n", numero / 1);
+  numero %= 1;
+
   return 0;
 }
