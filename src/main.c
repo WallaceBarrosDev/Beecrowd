8a10ddf9cf0eeb69ecc3a7d78fcf5e4e765a4322
WallaceBarrosDev <w.wallacebarro.cruz@gmail.com>
2026-04-11 16:03:43 -0300

    1019

--- src/main.c
+++ src/main.c
@@ -1,30 +1,15 @@
 #include <stdio.h>
 
 int main(void) {
-  int numero;
-  scanf("%d", &numero);
+  int tempo, h, m, s;
+  scanf("%d", &tempo);
 
-  printf("%d\n", numero);
-  printf("%d nota(s) de R$ 100,00\n", numero / 100);
-  numero %= 100;
+  h = tempo / 3600;
+  tempo %= 3600;
+  m = tempo / 60;
+  s = tempo % 60;
 
-  printf("%d nota(s) de R$ 50,00\n", numero / 50);
-  numero %= 50;
-
-  printf("%d nota(s) de R$ 20,00\n", numero / 20);
-  numero %= 20;
-
-  printf("%d nota(s) de R$ 10,00\n", numero / 10);
-  numero %= 10;
-
-  printf("%d nota(s) de R$ 5,00\n", numero / 5);
-  numero %= 5;
+  printf("%d:%d:%d\n", h, m, s);
   
-  printf("%d nota(s) de R$ 2,00\n", numero / 2);
-  numero %= 2;
-  
-  printf("%d nota(s) de R$ 1,00\n", numero / 1);
-  numero %= 1;
-
   return 0;
 }
