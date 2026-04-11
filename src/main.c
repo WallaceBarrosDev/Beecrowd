37ec96613f732b4fc4cf0cd5d18f0852dba53b44
WallaceBarrosDev <w.wallacebarro.cruz@gmail.com>
2026-04-10 22:21:01 -0300

    1016

--- src/main.c
+++ src/main.c
@@ -1,21 +1,8 @@
 #include <stdio.h>
-#include <math.h>
-
-double quadrado(double x);
 
 int main(void) {
-  double x1, y1, x2, y2, distancia;
-
-  scanf("%lf %lf", &x1, &y1);
-  scanf("%lf %lf", &x2, &y2);
-
-  distancia = sqrt(quadrado(x2 - x1) + quadrado(y2 - y1));
-
-  printf("%.4lf\n", distancia);
-
+  int km;
+  scanf("%i", &km);
+  printf("%i minutos\n", km*2);
   return 0;
-}
-
-double quadrado(double x) {
-  return x*x;
 }
