caefd10058c8d55c520b4e4696a153af1f039cd4
WallaceBarrosDev <w.wallacebarro.cruz@gmail.com>
2026-04-14 13:14:16 -0300

    1036

--- src/main.c
+++ src/main.c
@@ -1,10 +1,30 @@
-/* 1035 */
+/* 1036 */
 #include <stdio.h>
+#include <math.h>
 
 int main(void) {
-  int a, b, c, d;
-  scanf("%d %d %d %d", &a, &b, &c, &d);
+  double a, b, c, delta, raiz, r1, r2;
+  scanf("%lf %lf %lf", &a, &b, &c);
 
-  printf("%s\n", (b > c && d > a && (c+d) > (a+b) && c>=0 && d>=0 && a%2 == 0) ? "Valores aceitos" : "Valores nao aceitos");
+  delta = b*b - 4*a*c;
+  
+  if(delta < 0 || a == 0) {
+    printf("Impossivel calcular\n");
+    return 0;
+  }
+  
+  raiz = sqrt(delta);
+
+  if(raiz == 0) {
+    r1 = (-1*b)/(2*a);
+    r1 = r2;
+  } else {
+    r1 = (-1*b+raiz)/(2*a);
+    r2 = (-1*b-raiz)/(2*a);
+  }
+  
+  printf("R1 = %.5f\n", r1);
+  printf("R2 = %.5f\n", r2);
+
   return 0;
 }
