48d88f81c435b57fcde1f703626aef981a885156
WallaceBarrosDev <w.wallacebarro.cruz@gmail.com>
2026-04-15 15:00:41 -0300

    1078

--- src/main.c
+++ src/main.c
@@ -1,14 +1,12 @@
-/* 1075 */
+/* 1078 */
 #include <stdio.h>
 
 int main(void) {
-  int n;
-  scanf("%d", &n);
+  int N;
+  scanf("%d", &N);
 
-  for(int i = 0; i <= 10000; i++) {
-    if(i%n == 2)
-      printf("%d\n", i);
+  for (int i = 1; i <= 10; i++) {
+    printf("%d x %d = %d\n", i, N, i*N);
   }
-
   return 0;
 }
