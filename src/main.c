4ed28aae54d26834491fe96f4a84df6d1c50b97b
WallaceBarrosDev <w.wallacebarro.cruz@gmail.com>
2026-04-15 15:28:30 -0300

    1180

--- src/main.c
+++ src/main.c
@@ -1,12 +1,24 @@
-/* 1078 */
+/* 1180 */
 #include <stdio.h>
 
 int main(void) {
-  int N;
-  scanf("%d", &N);
+  int tamanho_array, posicao, menor_valor;
+  scanf("%d", &tamanho_array);
+  int array[tamanho_array];
 
-  for (int i = 1; i <= 10; i++) {
-    printf("%d x %d = %d\n", i, N, i*N);
+  for(int i = 0; i < tamanho_array; i++) {
+    scanf("%d", &array[i]);
+    if(i == 0) {
+      menor_valor = array[i];
+    } 
+
+    if(array[i] <= menor_valor) {
+      menor_valor = array[i];
+      posicao = i;
+    }
   }
+
+  printf("Menor valor: %d\nPosicao: %d\n", menor_valor, posicao);
+
   return 0;
 }
